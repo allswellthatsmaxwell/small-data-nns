8fc2041f3bf8282fb032d3f802ae0abb4f2e4d71
Maxwell <allswellthatsmaxwell9@gmail.com>
2020-09-04 14:36:09 -0500

    added docstrings and keras model

--- py/small.py
+++ py/small.py
@@ -1,9 +1,10 @@
 import os
 from sklearn import preprocessing, model_selection
 import pandas as pd
+import numpy as np
 from enum import Enum
 import random
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 class Splits(Enum):
     trn = 'train'
@@ -16,7 +17,10 @@ class DataHandler:
     def __init__(self, fname):
         self.csv = os.path.join(self.data_dir, fname)
 
-    def read(self):
+    def read(self) -> pd.DataFrame:
+        """
+        Reads this handler's data file from disk. 
+        """
         df = pd.read_csv(self.csv)
         self.original_predictors = [col for col in df.columns
                                     if col != self.response_var]
@@ -41,7 +45,9 @@ class DataHandler:
                                    if col != self.response_var]
         return df
 
-    def get_predictor_colnames(self):
+    def get_predictor_colnames(self) -> List[str]:
+        """ returns the predictors that match the columns (including order)
+        of the numpy matrix X returned elsewhere. """
         try:
             return self.encoded_predictors
         except AttributeError:
@@ -62,7 +68,8 @@ class DataHandler:
         df[scalecols] = self.standardizer.transform(df[scalecols])
         return df
 
-    def split_predictors_response(self, df):
+    def split_predictors_response(self, df) -> Tuple[pd.DataFrame, pd.Series]:
+        """ returns the predictor matrix and response series, as pandas objects. """
         predictors = self.get_predictor_colnames()
         X = df[predictors]
         y = df[self.response_var]
@@ -77,14 +84,23 @@ class DataHandler:
             datasets[name] = subdict
         return datasets
 
-    def apply_transforms(self, df):
+    def apply_transforms(self, df) -> Tuple[np.array, np.array]:
+        """
+        Applies one-hot-encoding and scaling to the variables of this data,
+        and returns the predictor matrix and response array as numpy arrays 
+        (2D and 1D, respectively).
+        """
         df = self.one_hot_encode(df)
         df = self.standardize(df)
         X, y = self.split_predictors_response(df)
         return X.values, y.values
     
-    def split(self, df):
-        random.seed(4)
+    def split(self, df, seed=4):
+        """
+        Splits df by row into train, validation, and test sets, using
+        the proportions in the Splits enum.
+        """
+        random.seed(seed)
         trn, valtst = model_selection.train_test_split(
             df, test_size = 1 - self.data_props[Splits.trn])
         val, tst = model_selection.train_test_split(
@@ -92,6 +108,9 @@ class DataHandler:
         return trn, val, tst
 
 class AbaloneHandler(DataHandler):
+    """
+    Handles data for the Abalone dataset from the UCI Machine Learning repository.
+    """
     def __init__(self):
         self.one_hot_vars = ['sex']
         self.response_var = 'rings'
